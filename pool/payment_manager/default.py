b1670d2ff8ee810c045f3cd859164eeb65f94831
Jack Nelson <jack@jacknelson.xyz>
2021-09-30 16:04:00 -0400

    add new rpc wallet to payment system

--- pool/payment_manager/default.py
+++ pool/payment_manager/default.py
@@ -508,7 +508,7 @@ class DefaultPaymentManager(AbstractPaymentManager):
         while True:
             try:
                 peak_height = self._state_keeper.blockchain_state["peak"].height
-                await self._wallet_rpc_client.log_in_and_skip(fingerprint=self.pps_wallet_fingerprint)
+                await self._pps_wallet_rpc_client.log_in_and_skip(fingerprint=self.pps_wallet_fingerprint)
                 if not self._state_keeper.blockchain_state["sync"]["synced"] or not self._state_keeper.wallet_synced:
                     self._logger.warning("Waiting for wallet sync")
                     await asyncio.sleep(60)
@@ -524,7 +524,7 @@ class DefaultPaymentManager(AbstractPaymentManager):
                 # blockchain_fee = 0.00001 * (10 ** 12) * len(payment_targets)
                 blockchain_fee: uint64 = uint64(0)
                 try:
-                    transaction: TransactionRecord = await self._wallet_rpc_client.send_transaction_multi(
+                    transaction: TransactionRecord = await self._pps_wallet_rpc_client.send_transaction_multi(
                         self.pps_wallet_id, payment_targets, fee=blockchain_fee
                     )
                 except ValueError as e:
@@ -539,7 +539,8 @@ class DefaultPaymentManager(AbstractPaymentManager):
                         not transaction.confirmed
                         or not (peak_height - transaction.confirmed_at_height) > self.confirmation_security_threshold
                 ):
-                    transaction = await self._wallet_rpc_client.get_transaction(self.pps_wallet_id, transaction.name)
+                    transaction = \
+                        await self._pps_wallet_rpc_client.get_transaction(self.pps_wallet_id, transaction.name)
                     peak_height = self._state_keeper.blockchain_state["peak"].height
                     self._logger.info(
                         f"Waiting for pps transaction to obtain {self.confirmation_security_threshold} confirmations"
