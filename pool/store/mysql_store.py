a43f0458e7ee1a4f0c252cc6762e91e88bb75ff1
Jack Nelson <jack@jacknelson.xyz>
2021-10-01 20:21:54 -0400

    fix

--- pool/store/mysql_store.py
+++ pool/store/mysql_store.py
@@ -340,7 +340,7 @@ class MySQLPoolStore(AbstractPoolStore):
                 payout = payment_target["amount"]
                 cursor = await connection.cursor()
                 await cursor.execute(f"SELECT launcher_id from farmer where payout_instructions=%s",
-                                     (payout_instructions.hex()))
+                                     (payout_instructions))
                 row = await cursor.fetchone()
                 launcher_id = row[0]
                 await cursor.close()
@@ -349,7 +349,7 @@ class MySQLPoolStore(AbstractPoolStore):
                     f"INSERT INTO payments(payout_time,block_height,transaction_id,launcher_id,payout_instructions,"
                     f"payout) "
                     f"VALUES(SYSDATE(6),%s,%s,%s,%s,%s)",
-                    (block_confirmed, transaction_id.hex(), launcher_id, payout_instructions.hex(), payout)
+                    (block_confirmed, transaction_id.hex(), launcher_id, payout_instructions, payout)
                 )
                 await connection.commit()
                 await cursor.close()
